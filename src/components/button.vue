aba32e85d73b374dddd5b13c228ae6ca1664a005
TW-153179-01\Joyce <e451745172001@gmail.com>
2023-08-19 16:30:47 +0800

    修改scss設立變數(增加wrapper)

---
+++ src/components/button.vue
@@ -0,0 +1,31 @@
+<!-- 如果要看scss引入可以看這個檔案 -->
+<template>
+    <div class="wrapper">
+        <div class="btn">
+            hi
+            <button>嗨嗨嗨嗨嗨嗨嗨</button>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+// @import '../assets/scss/global.scss';
+
+
+// @include btn();
+.btn{
+    @include btn(red,green,$h4);
+}
+// button才可以用點擊的部分
+button{
+    &:active{
+        background: #000;
+    }
+}
+
+// wrapper
+.wrapper{
+    @include wrapper();
+}
+
+</style>
