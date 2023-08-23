20ea14b75968e58def8f30a94c7b7b49cf971400
TW-153179-01\Joyce <e451745172001@gmail.com>
2023-08-24 01:33:31 +0800

    修改圖片尺寸及跑版跟版面顯示

--- src/components/shop.vue
+++ src/components/shop.vue
@@ -1,74 +1,77 @@
-<template>
-    <div class="card container-fluid">
-        <!----gx 跟gy為水平跟垂直距離-後面數字的是調整距離------------->
-        <div class="card-body row  gx-2 gy-3">
+<template> 
+   
+        <div class="card container-fluid">
+            <!----gx 跟gy為水平跟垂直距離-後面數字的是調整距離------------->
+            <div class="card-body row  gx-2 gy-3">
 
-            <nav class="navbar navbar-expand-lg navbar-light">
-                <div class="container-fluid">
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item over">
-                                <a class="nav-link active" aria-current="page" href="#">商品訂購</a>
-                            </li>  
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <div class="container-fluid">
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item over">
+                                    <a class="nav-link active" aria-current="page" href="#">商品訂購</a>
+                                </li>  
 
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">乳酪蛋糕</a>
-                            </li>
-                        </ul>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">乳酪蛋糕</a>
+                                </li>
+                            </ul>
+                        </div>
+                    
+                </nav>
+                
+
+                <div v-for="product in products" :key="product.id"  class="product col-3 ">
+                    <div class="img">
+                        <img :src="product.photos?.url || 'fallback-image.png'" class="card-img-top" alt="...">
+                        <button type="button" class="caption" data-bs-toggle="modal" :data-bs-target="'#exampleModal1'">
+                            加入購物車
+                        </button>
                     </div>
-                </div>
-            </nav>
-            
-            <div v-for="product in products" :key="product.id" style="width: 18rem;" class="product">
-                <div class="img">
-                    <img :src="product.photos?.url || 'fallback-image.png'" class="card-img-top" alt="...">
-                    <button type="button" class="caption" data-bs-toggle="modal" :data-bs-target="'#exampleModal1'">
-                        加入購物車
-                    </button>
-                </div>
 
-                <div class="description">
-                    <p class="card-text text-center">{{ product.name }}</p>
-                    <p class="card-text text-center price">NT{{ product.specs[0].price }}</p>
-                </div>
-            </div> 
+                    <div class="description">
+                        <p class="card-text text-center">{{ product.name }}</p>
+                        <p class="card-text text-center price">NT{{ product.specs[0].price }}</p>
+                    </div>
+                </div> 
 
 
 
+            </div>
         </div>
-    </div>
 
-    <!-- Modal (彈窗)-->
-    <div class="modal fade " id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal"
-                        aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    ...
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary"
-                        data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+        <!-- Modal (彈窗)-->
+        <div class="modal fade " id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                            aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary"
+                            data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
 
-    <div class="container mt-3 ">
-        <ul class="pagination justify-content-center">
-            <li class="page-item"><a class="page-link active" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item"><a class="page-link" href="#">4</a></li>
-            <li class="page-item"><a class="page-link" href="#">5</a></li>
-            <li class="page-item"><a class="page-link" href="#">></a></li>
-        </ul>
+        <div class="container mt-3 ">
+            <ul class="pagination justify-content-center">
+                <li class="page-item"><a class="page-link active" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item"><a class="page-link" href="#">4</a></li>
+                <li class="page-item"><a class="page-link" href="#">5</a></li>
+                <li class="page-item"><a class="page-link" href="#">></a></li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -106,6 +109,7 @@
 /*----------商品訂購< 乳酪蛋糕---------------------------------------*/
 
 
+
 .over ::after{
     content: " <";
 }
@@ -117,15 +121,19 @@
 .card-img-top:hover {
     opacity: 0.75;
 }
+/* 最外面的% */
 .container-fluid{
+    max-width: 1200px;
     margin: 0 auto;
+    border: none;
 }
 
 
 .product{
     /* 左右分配 */
     /* flex-grow: 1;第二排平均分配 */
-    margin-left: 1.1%;
+
+    /* margin-left: 1.1%; */
 
 }
 
@@ -135,18 +143,21 @@
     position: relative;
     overflow: hidden;
     width: 100%;
-    height: 60%;
+    height: 40vh;
     /* object-position: center center; */
-    display: block;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
 }
 
 .card-img-top{
-    width: 100%;
-    height: 100%;
-    /* display: block; */
+    min-width: 100%;
+    min-height: 100%;
+    /* disp*lay: block; */
     margin: auto 0;
 }
+
 
 .caption {
     width: 100%;
